8d4930f3b08a5eb24b8747bdd97dbb4cf3628d51
ethbonney <98558091+EthanBonney@users.noreply.github.com>
2022-11-15 19:10:22 -0500

    1

--- client/src/stores/Users.ts
+++ client/src/stores/Users.ts
@@ -2,25 +2,13 @@ import { reactive } from "vue";
 
 const session = reactive( {
     user: null as User | null,
-    
-    get token() {
-        return this.user?.token;
-    },
-
-    set token( token: string | undefined)
-    {
-        if (this.user)
-        {
-            this.user.token = token;
-        }
-    }
 });
 
-export function login(firstName: string, lastName: string, token: string) {
+export function login(name: string, email: string, password: string) {
     session.user = {
-        firstName,
-        lastName,
-        token,
+        name,
+        email,
+        password,
     };
 }
 
@@ -28,15 +16,10 @@ export function logout() {
     session.user = null;
 }
 
-export function userToken()
-{
-    return session.user?.token;
-}
-
-export class User {
-    public firstName?: string;
-    public lastName?: string;
-    public token?: string | undefined;
+export interface User {
+    name: string;
+    email: string;
+    password?: string;
 }
 
 export default session;
